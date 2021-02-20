ecc7599146db4d47a227fc304df8c3cde4851d04
Toriong <67920345+Toriong@users.noreply.github.com>
2021-02-19 23:58:35 -0600

    delete and complete button are functioning
    The user is now able to designate a task as 'complete' in the app, and by doing so, will move the task to the completion list. The user is also able to delete items from either the to-do list or the completion list.

--- index.js
+++ index.js
@@ -8,46 +8,96 @@ import React, {useState, useReducer }from 'react'
 
 const initialState = {
     toDoTasks: [],
-    toDoNames: [],
+    completion: []
 }
 const reducer = (previousToDoItems, action) => {
-    console.log(action)
     if (action.type === "CONFIRMED_TO_DO") {
         const newTask = action.confirmedToDo;
         return {
             ...previousToDoItems,
-            toDoTasks: [newTask],
-            toDoNames: [action.confirmedToDo.Task]
+            toDoTasks: [...previousToDoItems.toDoTasks, newTask],
         }
+    } else if (action.type === "COMPLETED") {
+        console.log(action.payload)
+        let completedItem = action.payload
+        console.log("ID: ",action.payload.id)
+        var upDatedToDoTasks = previousToDoItems.toDoTasks.filter((item) => {
+            console.log("hello",item.id);
+            return item.id !== action.payload.id
+        })
+        console.log(upDatedToDoTasks);
+        return {
+            ...previousToDoItems,
+            toDoTasks: upDatedToDoTasks,
+            completion: [...previousToDoItems.completion, completedItem]
+        }
+    } else if (action.type === "DELETE") {
+        let toDoTasksCheck = previousToDoItems.toDoTasks.includes(action.payload);
+        let completionCheck = previousToDoItems.completion.includes(action.payload);
+        console.log("to-do check",toDoTasksCheck);
+        console.log("completion check",completionCheck);
+        if(toDoTasksCheck === true)
+        {
+            var upDatedToDoTasks = previousToDoItems.toDoTasks.filter((tasks) => {
+            return tasks.id !== action.payload.id;
+        })
+            return {
+                ...previousToDoItems,
+                toDoTasks: upDatedToDoTasks,
+            
+        } 
+            } else if (completionCheck === true) {
+            let updatedCompletion = previousToDoItems.completion.filter((item) => {
+                    return item.id !== action.payload.id
+                })
+                return {
+                    ...previousToDoItems,
+                    completion: updatedCompletion
+                }
+        }
+        ;
     }
 }
 //WHAT I WANT: when the user presses the add button, have the item first be entered into the toDoArray and then have a function that will put that item onto the UI. 
-
+// WHAT I WANT: form the array of objects, I want the value of the task property to be displayed onto the UI
 
 
 const Index = () => {
-    const DomManipulation = (task) => {
-    return (
-        <>
-            <li>{task}<button>COMPLETED</button><button>DEL</button></li> 
-        </>
-    );
-}
+//     const DomManipulation = (task) => {
+//     return (
+//         <>
+//             <li>{task}<button>COMPLETED</button><button>DEL</button></li> 
+//         </>
+//     );
+// }
+    // functionality of the deleteItem fucntion
+    // GOAL: when the user presses the completed button, have the item be deleted from the toDoArray and be brought to the compArray
+    // 1. get the object from the toDoArray and the id
+    // 2. put each into its respective variable  
+    // 3. use the id to run a filter through the toDoArray to delete the specific item from the toDoArray
+    // 4. spit the new array out set it equal to the toDoArray.
+    //5. have the variable that stored the object that is set as completed be put into the completed Array. 
     const [toDoItem, setToDoItem] = useState('');
     const [currentToDoItems, dispatch] = useReducer(reducer, initialState);
-    const AddToDo = (e) => {
-        e.preventDefault();
-        console.log('hello')
+    const AddToDo = (reset) => {
+        reset.preventDefault();
         let confirmedToDo = { id: new Date().getTime().toString(), Task: toDoItem, completed: false };
-        console.log(confirmedToDo)
-        dispatch({ type: ("CONFIRMED_TO_DO"), confirmedToDo })
+        setToDoItem(" ")
+        dispatch({ type: ("CONFIRMED_TO_DO"), confirmedToDo });
+    }
+    
+    const CompletedItem = (task) => {
+            dispatch(({type:"COMPLETED", payload: task}))    
+    }
+    const DeleteMe = (task) => {
+        dispatch({type: "DELETE", payload: task})
     }
     return (
         <>
         <div className="to-do-list-adder">
                 <h5>Add Task</h5>
                 <form onSubmit={AddToDo}>      
-                <input type="text" value={toDoItem} onChange={(typing) => { setToDoItem(typing.target.value) }}/>
+                    <input type="text" value={toDoItem} onChange={(typing) => { setToDoItem(typing.target.value) }} />
                 <br/>
                 <br/>
                 <button type="submit" onSubmit={AddToDo} >ADD</button>
@@ -55,18 +105,21 @@ const Index = () => {
             </div>
 
             <header>
+                {/* {console.log(currentToDoItems.toDoTasks)} */}
                 <div className="to-do-list-container">
                     <h1>My To-do List</h1>
                     <div className="user-confirmed-to-dos-container">
-                        {console.log(currentToDoItems)}
-                        {console.log(currentToDoItems.toDoArray)}
                         <ul id="confirmed-to-do-list">
-                            {currentToDoItems.toDoNames.map((task) => {
-                                {console.log(task)}
-                                {/* WHAT I WANT: have each task undergo the DOM manipulation. make a function that will recieve the task, perform the DOM manipulation upon the task, and spit it out */ }
+                            {currentToDoItems.toDoTasks.map((task) => {
+                                { console.log(currentToDoItems.toDoTasks) }
+                                { console.log("user to dos:", task.Task) }
+                                { console.log('toDoObject', task) }
                                 return (
                                     <>
-                                    <li>{task}<button>COMPLETED</button><button>DEL</button></li> 
+                                        <li
+                                            id={task.id}
+                                        >{task.Task}<button onClick={() => { CompletedItem(task) }}>COMPLETED</button><button onClick={() => { DeleteMe(task) }}>DEL</button>
+                                        </li> 
                                     </>
                                 )
                                     
@@ -76,7 +129,7 @@ const Index = () => {
 
                 </div>
 
-    </header>
+            </header>
     
     <div class="to-do-list-container">
     <div class="user-confirmed-to-dos-container">
@@ -88,13 +141,19 @@ const Index = () => {
     <div class="completion-containter">
         <h1>Your Completions. Great Job!</h1>
         <ol id="user-completion-list">
-        
+            {currentToDoItems.completion.map((completedItem) => {
+                return (
+                    <li>{completedItem.Task} <button onClick={() => { DeleteMe(completedItem) }}>DEL</button> </li>  
+                );
+        })}
     
     
         </ol>
-    
+        {console.log(currentToDoItems.completion)}
+        {console.log(currentToDoItems.toDoTasks)}
+        
     </div>
-    </div>
+            </div>
         </>
     )
 }
