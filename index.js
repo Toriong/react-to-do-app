91c81aadde8c5eaef6fb7b4190d72740e547ca3a
Toriong <67920345+Toriong@users.noreply.github.com>
2021-02-25 21:55:57 -0600

    modal for inline editing, 'completionArray' deleted
    If the user wants to edit any of the to-do tasks that are already on the to-do list UI, then the user can press on either the task or the due date, which will then bring a modal that has two inputs with the tasks and the due dates as its values. If the user made any edits, then the user can press confirm. If the user wants to disregard any changes/edits, then the user can press cancel. Still working on the styling for the modals.
    Deleted the 'completionArray'. The rendering of the DOM will be based upon the key name of 'completed' in each item in the 'tasksArray'. If 'completed: false', then the 'task value' of the object will be rendered onto the to-do list of the DOM. If 'completed: true', then the task of that object will be rendered onto the completion list of the DOM.

--- index.js
+++ index.js
@@ -1,99 +1,160 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Index = () => {
     const [toDoItem, setToDoItem] = useState('');
-    const [toDoItemsArray, setToDoItemsArray] = useState([])
     const [dueDate, setDueDate] = useState('')
-    const [completionArray, setCompletionArray] = useState([]);
+    const [userInputArray, setUserInputArray] = useState([])
+
     const AddToDo = (reset) => {
         reset.preventDefault()
-        let confirmedToDo = { id: new Date().getTime().toString(), task: toDoItem, completed: false, dueDate: dueDate};
-        setToDoItemsArray(previousArray => [...previousArray, confirmedToDo]);
+        let confirmedToDo = { id: new Date().getTime().toString(), task: toDoItem, completed: false, dueDate: dueDate };
+        setUserInputArray(previousArray => [...previousArray, confirmedToDo]);
         setToDoItem(" ");
         setDueDate(" ");
     };
-    const toDoTaskMarkedAsCompleted = (item) => {
-        let completedItem;
-        if (item.completed === false) {
-            completedItem = { id: item.id, task: item.task, dueDate: item.dueDate, completed: true }
-        }
-        return completedItem;
-    }
-    const ItemCompleted = (completion) => {
-        let completedItem = toDoTaskMarkedAsCompleted(completion)
-        let upDatedToDoTasks = toDoItemsArray.filter((toDo) => {
-            return toDo.id !== completion.id
-        })
-        setToDoItemsArray(upDatedToDoTasks);
-        setCompletionArray(previousCompletions => [...previousCompletions, completedItem])
-    }
-    const DeleteMe = (item, array, fn) => {
-        let updatedArray = array.filter((deleteItem)=>{
+
+    const DeleteMe = (item) => {
+        let updatedArray = userInputArray.filter((deleteItem) => {
             return deleteItem.id !== item.id
         })
-        fn(updatedArray);
+        setUserInputArray(updatedArray);
     }
-    
-    useEffect(() => {
-        const getToDoTasks = JSON.parse(localStorage.getItem("Saved-To-Do-Tasks"));
-        const getCompletions = JSON.parse(localStorage.getItem("Saved-Completions"));
-        setToDoItemsArray(getToDoTasks);
-        setCompletionArray(getCompletions);
 
-    },[])
+    const Task = ({ taskItem }) => {
+        const userItem = {
+            task: taskItem.task,
+            dueDate: taskItem.dueDate
+        }
+
+        const completedItem = () => {
+            let upDatedArray = userInputArray.map((item) => {
+                if (item.id === taskItem.id) {
+                    return {
+                        ...item,
+                        completed: true
+                    }
+                } else {
+                    return item;
+                }
+            })
+            setUserInputArray(upDatedArray)
+        }
+
+        const [tasks, setTasks] = useState(userItem);
+        const [editMe, setEditMe] = useState(false);
+
+        const edits = (event) => {
+            let value = event.target.value;
+            let name = event.target.name;
+            setTasks({ ...tasks, [name]: value })
+        }
+
+        const clickToEdit = () => {
+            setEditMe(!editMe);
+        }
+
+        const getObject = () => {
+            for (let i = 0; i <= userInputArray.length; i++) {
+                if (userInputArray[i].id === taskItem.id) {
+                    return userInputArray[i];
+                }
+            }
+        }
+        const confirmChange = () => {
+            let targetObject = getObject();
+            targetObject.task = tasks.task
+            targetObject.dueDate = tasks.dueDate
+            let index = userInputArray.indexOf(taskItem)
+            userInputArray[index] = targetObject
+            localStorage.setItem("User-Input-Array", JSON.stringify(userInputArray))
+            setEditMe(false)
+        }
+
+        const cancel = () => {
+            tasks.task = taskItem.task
+            tasks.dueDate = taskItem.dueDate
+            setEditMe(false);
+        }
+
+        return (
+            <>
+                {editMe ?
+                    <div className="edit-item-modal">
+                        <input id="userTask" type="text" defaultValue={tasks.task} name="task" onChange={edits} />
+                        <input id="userDate" type="date" defaultValue={tasks.dueDate} name="dueDate" onChange={edits} />
+                        <button type="button" onClick={() => confirmChange()}>CONFIRM CHANGES</button>
+                        <button type="button" onClick={() => cancel()}>CANCEL</button>
+                    </div>
+                    :
+                    <li>
+                        <span>
+                            <div onClick={() => clickToEdit()}>{taskItem.task}</div>
+                            <div onClick={() => clickToEdit()}>DUE ON: {taskItem.dueDate}</div>
+                            <button onClick={completedItem}>COMPLETED</button><button onClick={() => DeleteMe(taskItem)}>DEL</button>
+                        </span>
+                    </li>
+                }
+            </>
+        );
+    }
 
     useEffect(() => {
-        const toDoTasks = JSON.stringify(toDoItemsArray)
-        const completions = JSON.stringify(completionArray)
-        localStorage.setItem("Saved-To-Do-Tasks", toDoTasks);
-        localStorage.setItem("Saved-Completions", completions);
-    })
+        let getItems = JSON.parse(localStorage.getItem("User-Input-Array"));
+        setUserInputArray(getItems);
+    }, [])
+
+    useEffect(() => {
+        let save = JSON.stringify(userInputArray)
+        localStorage.setItem("User-Input-Array", save)
+    }, [userInputArray])
+
 
     return (
         <>
-        <div className="to-do-list-adder">
+            <div className="to-do-list-adder">
                 <h5>Add Task</h5>
-                <form onSubmit={AddToDo}>      
+                <form onSubmit={AddToDo}>
                     <input type="text" value={(toDoItem)} onChange={(typing) => { setToDoItem(typing.target.value) }} />
+                    <h5>DUE ON: </h5>
+                    <input type="date" value={(dueDate)} onChange={(typing) => { setDueDate(typing.target.value) }}></input>
                     <br />
-                    <br/>
+                    <br />
                     <button onSubmit={AddToDo}>ADD</button>
                 </form>
-        </div>
-
-            <header>
-                <div className="to-do-list-container">
-                    <h1>My To-do List</h1>
-                    <div className="user-confirmed-to-dos-container">
-                        <ul id="confirmed-to-do-list">
-                            {console.log("line 112", toDoItemsArray)}
-                            {toDoItemsArray.map((item) => {
-                                return (
-                                        <li>{item.task} <button onClick={() => ItemCompleted(item)}>✅</button> <button onClick={()=>DeleteMe(item, toDoItemsArray, setToDoItemsArray)}>🗑️</button> DUE ON: <input type="date" value={(dueDate)} onChange={(typing) => { setDueDate(typing.target.value) }}></input></li>  
-                                );
-                            })}
-                        </ul>
-                    </div>     
-                        </div>    
-                </header>
-    <div class="to-do-list-container">
-    <div class="user-confirmed-to-dos-container">
-        <ul id="confirmed-to-do-list">
-        </ul>
-    </div>
-    <div class="completion-containter">
-        <h1>Your Completions. Great Job!</h1>
-        <ol id="user-completion-list">
-            {completionArray.map((item) => {
-                return (
-                    <li>{item.task}<button onClick={() => DeleteMe(item, completionArray, setCompletionArray)}>🗑️</button></li>  
-                );   
-            })}
-        </ol>
-        
-    </div>
             </div>
-            {console.log(toDoItemsArray)}
+            <h1>My To-do List</h1>
+            <div id="confirmed-to-do-list">
+                <ul className="to-do-list">
+                    {userInputArray.map((item) => {
+                        if (item.completed === false) {
+                            return (
+                                <Task taskItem={item} />
+                            )
+                        } else {
+                            return null;
+                        }
+                    })}
+                </ul>
+            </div>
+            <div class="user-confirmed-to-dos-container">
+            </div>
+            <div class="completion-containter">
+                <h1>Your Completions. Great Job!</h1>
+                <ul id="user-completion-list">
+                    {userInputArray.map((item) => {
+                        if (item.completed === true) {
+                            return (
+                                <li>{item.task}<button onClick={() => DeleteMe(item)}>DEL</button></li>
+                            )
+                        } else {
+                            return null;
+                        }
+                    })}
+                </ul>
+
+            </div>
         </>
-    )}
+    )
+}
+
 export default Index;
